b216a316350b9cc7bb6eed657d6a80bcb125fca5
Onion Limited <onionltd@protonmail.com>
2020-02-24 16:53:35 +0000

    Refactor popups code

--- src/js/origin/tor2web.js
+++ src/js/origin/tor2web.js
@@ -1,5 +1,12 @@
 import {OriginBase} from "./base.js";
-import {Popup, Tor2WebPopup} from "../popup.js";
+import {Popup} from "../popup.js";
+
+const Tor2WebPopup = {
+    "title": "CAUTION: Using Tor2Web Proxy!",
+    "icon": Popup.icon("bird_dead"),
+    "file": "popup/tor2web.html",
+    "data": {}
+};
 
 export class OriginTor2Web extends OriginBase {
     constructor() {
