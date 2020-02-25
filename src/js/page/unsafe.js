8e2fa8279acdcb17159a8330f0d2f7fb473cff63
Onion Limited <onionltd@protonmail.com>
2020-02-25 13:03:07 +0000

    Update setButtonAction to accept two different selectors

--- src/js/page/unsafe.js
+++ src/js/page/unsafe.js
@@ -9,12 +9,12 @@ class Page extends PageBase {
 
 new Page(new URL(window.location.href).searchParams.get("url")).render();
 
-Page.setButtonAction("report", function(){
+Page.setButtonAction({ "id": "report" }, function(){
     let title = "Phishing site " + new URL(window.location.href).searchParams.get("url");
     window.open("https://github.com/onionltd/oniontree/issues/new?labels=report-phishing&title=" + encodeURIComponent(title),
         '_blank').focus();
 });
-Page.setButtonAction("recommend", function(){
+Page.setButtonAction({ "id": "recommend" }, function(){
     let title = "New service " + new URL(window.location.href).searchParams.get("url");
     window.open("https://github.com/onionltd/oniontree/issues/new?labels=new-service&title=" + encodeURIComponent(title),
         '_blank').focus();
